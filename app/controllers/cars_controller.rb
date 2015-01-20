1db98eede961a516e0d326d8055fcaee9c7bf692
William Griffin <williamgriffin989@gmail.com>
2015-01-20 15:38:35 -0500

    defined show

--- app/controllers/cars_controller.rb
+++ app/controllers/cars_controller.rb
@@ -1,2 +1,26 @@
 class CarsController < ApplicationController
+	def new 
+		@newcar = Car.new
 end
+
+	def create
+		@car = Car.create car_params
+		redirect_to root_path
+	end
+
+	def car_params
+		params.require(:car).permit(
+			:Make,
+			:Model,
+			:Year,
+			:Color
+			)
+	end
+
+	def show
+		@car = Car.find params[:id]
+	end
+
+	
+end
+
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -1,5 +1,6 @@
 class HomeController < ApplicationController
 	def index 
+		@cars = Car.all
 	end 
 end
 
